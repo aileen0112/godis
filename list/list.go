cdabb373938c673e83fa59734f4babebe7a88cd8
alphali <alphalizhi@gmail.com>
2018-04-02 22:24:50 +0800

    list struct

---
+++ list/list.go
@@ -0,0 +1,13 @@
+package list
+
+type List struct {
+}
+
+func Add(node Node) error {
+}
+func Length(list List) int {
+}
+func First(list List) (Node, error) {
+}
+func Last(list List) (Node, error) {
+}
