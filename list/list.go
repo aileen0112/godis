68c8504998330acb231457b582cf824e0373e6ab
alphali <alphalizhi@gmail.com>
2018-04-03 09:47:50 +0800

    implement of list from package list

--- list/list.go
+++ list/list.go
@@ -1,13 +1,54 @@
 package list
 
+// List includes root element and length of list
 type List struct {
+	Root Element
+	Len  int
 }
 
-func Add(node Node) error {
+// Element is the list's elemennt
+type Element struct {
+	next  *Element
+	prev  *Element
+	list  *List
+	value interface{}
 }
-func Length(list List) int {
+
+// Create return a new list
+func Create() *List {
+	return new(List)
 }
-func First(list List) (Node, error) {
+
+/*
+func Init() *List {
 }
-func Last(list List) (Node, error) {
+*/
+
+// AddHead insert value v to the head of list
+func (l *List) AddHead(v interface{}) *Element {
+	return l.insert(&Element{value: v}, &l.Root)
 }
+
+//func AddTail(node Node) error {}
+func (l *List) insert(e, at *Element) *Element {
+	n := at.next
+	at.next = e
+	e.next = n
+	e.prev = at
+	n.prev = e
+	e.list = l
+	l.Len++
+	return e
+}
+
+// Len return the length of list
+func Len(l *List) int {
+	return l.Len
+}
+
+// First return the root element of list
+func (l *List) First() *Element {
+	return &l.Root
+}
+
+//func Last(list List) (Node, error) {}
